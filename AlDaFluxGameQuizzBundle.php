509b9d537a298682a4013f09d1ae218c8d809d27
AlDaFlux <antoine.lotz@gmail.com>
2020-01-06 11:46:17 +0100

    first commit

---
+++ AlDaFluxGameQuizzBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace AlDaFlux\GameQuizzBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AlDaFluxGameQuizzBundle extends Bundle
+{
+}
---
+++ Tests/Controller/DefaultControllerTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace AlDaFlux\GameQuizzBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class DefaultControllerTest extends WebTestCase
+{
+    public function testIndex()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/');
+
+        $this->assertContains('Hello World', $client->getResponse()->getContent());
+    }
+}
